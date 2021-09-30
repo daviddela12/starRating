41976182bfb3eaf74ed0d8405b4cb344665e2a9e
daviddela <daviddela>
2021-09-30 21:14:40 +0200

    product unit tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'product attributes must not be empty' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+  end
+
+  test 'product price must be positive' do
+    product = products(:frozen)
+    assert product.valid?
+  end
+
+  test 'product price must not be negative or zero' do
+    product = products(:frozen)
+    product.price = -1
+    assert product.invalid?
+    product.price = 0
+    assert product.invalid?
+  end
 end
