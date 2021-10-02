1a8d27755bd8a76e532b0988b072afbec967e2ac
daviddela <daviddela>
2021-10-02 11:34:24 +0200

    Created grid to show products in frontend

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,7 +1,16 @@
 class ProductController < ApplicationController
+  before_action :set_product, only: %i[ show ]
+
   def index
+    @products = Product.all.order('title')
   end
 
   def show
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+  root 'product#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
