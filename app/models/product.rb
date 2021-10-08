e28b52777273136d269d242ae8b7a7a7a8c8458a
daviddela <daviddela>
2021-10-08 18:33:15 +0200

    average of reviews in the model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,15 @@ class Product < ApplicationRecord
 
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, :description, :price, presence: true
+
+  def average_reviews
+    average = 0
+    unless reviews.empty?
+      reviews.each do |review|
+        average += review.rating
+      end
+      average /= reviews.count
+    end
+    average.round(1)
+  end
 end
